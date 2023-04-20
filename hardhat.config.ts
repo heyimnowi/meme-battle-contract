84de6f55580de9ba8ab418f293aea79865eb18e4
Noelia Lopez <noelia.l@toptal.com>
2023-04-20 17:49:18 +0200

    Added dotenv to hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,8 @@
 import { HardhatUserConfig, task } from "hardhat/config";
+import { config as dotenvConfig } from "dotenv";
 import "@nomicfoundation/hardhat-toolbox";
+
+dotenvConfig();
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -11,6 +14,17 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    goerli: {
+      // This value will be replaced on runtime
+      url: process.env.STAGING_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
+    mainnet: {
+      url: process.env.PRODUCTION_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
+  },
 };
 
 export default config;
