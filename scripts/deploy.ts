cdbf3ba3899741fdd8f78e221149d834966363a8
Noelia Lopez <noelia.l@toptal.com>
2023-04-20 19:18:14 +0200

    Removed the voting before the deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -22,18 +22,6 @@ async function main() {
 
   // Print the contract address
   console.log("Voting contract deployed to:", voting.address);
-
-  // Vote for an option
-  const optionIndex = 0;
-  const signer = (await ethers.getSigners())[0];
-  await voting.connect(signer).vote(optionIndex);
-
-  // Print the updated vote counts
-  console.log("Vote counts:");
-  for (let i = 0; i < options.length; i++) {
-    const option = await voting.options(i);
-    console.log(`${option.name}: ${option.count}`);
-  }
 }
 
 // Run the script
